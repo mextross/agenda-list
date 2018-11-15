4546faabe614157130a452ea0b3ecc6087db547f
Michelle Extross <michelleanneextross@yahoo.co.nz>
2018-11-15 15:16:28 +1300

    Created a new object engines

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Note from './components/Note'; //import Note component
+import headerlogo from './headerlogo.png';
+
 import './App.css';
 
 class App extends Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      noteText : '',
+      notes : [] //holds array of each note we want to create
+    }
+  }
+
+  updateNoteText(noteText){
+    //update the notetext as its changes
+    this.setState({
+      noteText : noteText.target.value //get the value of the textbox
+    })
+  }
+
+  handleKeyPress = (event) => { //pass event as the argument
+    if(event.key === 'Enter'){
+      this.addNote();
+    }
+  }
+  addNote(){
+    // console.log(this.state.noteText);
+    if(this.state.noteText === '') {return}
+    let notesArr = this.state.notes;
+
+    notesArr.push(this.state.noteText);
+   // console.log(notesArr);
+    //reset the state of the note textbox to empty after it has been added
+    this.setState({noteText : ''});
+    this.textInput.focus(); //set the mouse focus on the textbox after it has been added
+
+  }
+  deleteNote(index){
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1); //remove the note from the note array
+    //update note array with the new array set after deleting
+    this.setState({ notes : notesArr }); 
+  }
   render() {
+    let notes = this.state.notes.map((val, key) => {
+      console.log(key);
+      return <Note key={key} text={val} deleteMethod={ () => this.deleteNote(key) }/>
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+      <div className="container">
+        <div class="header">
+        <h1> <img src={headerlogo} /> Michelle's Todo List </h1>
+        </div>
+        {notes}
+        <div className="btn" onClick={ this.addNote.bind(this) }>
+          +
+        </div>
+        <input type="text" ref={ ((input) => {this.textInput = input} )}
+         className="textInput" value={this.state.noteText}
+          onChange={noteText => this.updateNoteText(noteText)}
+          onKeyPress={this.handleKeyPress.bind(this)}
+          /> 
       </div>
     );
   }
